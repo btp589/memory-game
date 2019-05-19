7dc6551eece22e709ec9788fe04fcbac16b78c85
bphan <btp589@gmail.com>
2019-05-19 14:58:52 -0700

    added files

---
+++ src/images.js
@@ -0,0 +1,29 @@
+import blackpanther from './images/black_panther.png';
+import blackwidow from './images/black_widow.png';
+import captainamerica from './images/captain_america.png';
+import docstrange from './images/doc_strange.png';
+import hulkarmor from './images/hulk_armor.png';
+import hulk from './images/hulk.png';
+import ironman2 from './images/iron_man2.png';
+import spiderman from './images/spider_man.png';
+import thanos from './images/thanos.png';
+import venom from './images/venom.png';
+import thor from './images/thor.png';
+import vision from './images/vision.png';
+
+const images = [
+  blackpanther,
+  blackwidow,
+  captainamerica,
+  docstrange,
+  hulkarmor,
+  hulk,
+  ironman2,
+  spiderman,
+  thanos,
+  venom,
+  thor,
+  vision
+];
+
+export default images;
